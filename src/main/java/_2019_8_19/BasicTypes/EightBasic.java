6a37c2f06bb0ca93610adcb291ecc71223ac2652
fys <1113014250@qq.com>
2019-08-20 19:06:58 +0800

    2019-8-20

--- src/main/java/_2019_8_19/BasicTypes/EightBasic.java
+++ src/main/java/_2019_8_19/BasicTypes/EightBasic.java
@@ -1,5 +1,7 @@
 package _2019_8_19.BasicTypes;
 
+import static java.lang.Float.*;
+
 public class EightBasic
 {
     public static void main(String[] args)
@@ -7,12 +9,12 @@ public class EightBasic
         /**
          * byte的取值范围是【-128,127】
          */
-        byte b;
-        b = (byte) 128;
-        System.out.println(b);
-
-        b = (byte) 129;
-        System.out.println(b);
+        //byte b;
+        //b = (byte) 128;
+        //System.out.println(b);
+        //
+        //b = (byte) 129;
+        //System.out.println(b);
         //byte a = -1;
         //System.out.println(a);
         //a = (byte) 255;
@@ -20,5 +22,14 @@ public class EightBasic
 
         //short s = (short) 32768;
         //System.out.println(s);
+        float positiveInfinity = POSITIVE_INFINITY;
+        float negativeInfinity = NEGATIVE_INFINITY;
+
+        //System.out.println(isNaN(1f / 0.0f));
+        float a = NaN;
+        System.out.println(a == a);
+
+        //float b = 1/0;
+        //System.out.println(b);
     }
 }
---
+++ src/main/java/_2019_8_20/Static.java
@@ -0,0 +1,8 @@
+package _2019_8_20;
+
+public class Static
+{
+    public static final int a = 1;
+
+    public static int b = 2;
+}
